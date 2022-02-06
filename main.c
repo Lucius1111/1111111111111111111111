b2584a90c7e6bcaa7dc48e5949d9e3457ee51a08
Roccer211 <danilca10@mail.ru>
2022-02-06 17:43:42 +0300

    testing

--- main.c
+++ main.c
@@ -1,10 +1,35 @@
 #include"libs/data_stuctures/vector/vector.h"
 
-int main() {
-    int n;
+void test_pushBack_emptyVector() {
+    vector v = createVector(0);
+    pushBack(&v, 42);
+    assert(v.data[0] == 42);
+    assert(v.capacity == 1);
+    assert(v.size == 1);
+    deleteVector(&v);
+}
+
+void test_pushBack_fullVector() {
     vector v = createVector(5);
-    reserve(&v,SIZE_MAX);
+    v.size = 5;
+    v.data[0] = 1;
+    v.data[0] = 2;
+    v.data[0] = 3;
+    v.data[0] = 3;
+    v.data[0] = 5;
+    pushBack(&v, 42);
+    assert(v.data[5] == 42);
+    assert(v.size == 6);
+    assert(v.capacity == 10);
+}
+
+void test(){
+    test_pushBack_emptyVector();
+    test_pushBack_fullVector();
+}
+
+int main() {
+    test();
 
     return 0;
-
 }
