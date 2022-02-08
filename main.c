dc980099a31181cdfc2c5194f8cbf6304250e28d
Roccer211 <danilca10@mail.ru>
2022-02-08 22:47:29 +0300

    append push / pop functions

--- main.c
+++ main.c
@@ -78,8 +78,22 @@ void test() {
 }
 
 int main() {
-    vectorVoid vI = (vectorVoid) {NULL, 0, 0, sizeof(int)};
-    vectorVoid vF = (vectorVoid) {NULL, 0, 0, sizeof(float)};
+
+
+    size_t n;
+    scanf("%zd", &n);
+    vectorVoid v = createVectorV(0, sizeof(float));
+    for (int i = 0; i < n; i++) {
+        float x;
+        scanf("%f", &x);
+        pushBackV(&v, &x);
+    }
+    for (int i = 0; i < n; i++) {
+        float x;
+        getVectorValueV(&v, i, &x);
+
+        printf("%f ", x);
+    }
 
     test();
 
