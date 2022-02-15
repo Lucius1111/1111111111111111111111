f520e31f3d2ddf0c37fde824e06d3fdb792863e0
Roccer211 <danilca10@mail.ru>
2022-02-15 22:41:28 +0300

    task 2

--- main.c
+++ main.c
@@ -1,13 +1,29 @@
 #include "libs/data_stuctures/matrix/matrix.h"
 #include "libs/algorithms/algorithm.h"
 
+int getMax(const int *a, int n) {
+    size_t maxIndex;
+    int max = a[0];
+    for (size_t i = 1; i < n; i++) {
+        if (a[i] > max) {
+            maxIndex = i;
+            max = a[i];
+        }
+    }
+    return max;
+}
+
+void sortRowsByMaxElement(matrix m) {
+    insertionSortRowsMatrixByRowCriteria(m, getMax);
+}
+
 int main() {
     size_t a, b;
     scanf("%zu %zu", &a, &b);
     matrix m = getMemMatrix(a, b);
     inputMatrix(m);
 
-    swapRows(m, getMinValuePos(m).rowIndex, getMaxValuePos(m).rowIndex);
+    sortRowsByMaxElement(m);
 
     outputMatrix(m);
 
